0c42c990061dc4b43ed615cbc09a50a749bbc834
zhixuan.duan <zduan@ritchiebros.com>
2024-04-28 13:36:36 +0800

    feat: init iOS project.

---
+++ BookMagagementApp/BookMagagementAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMagagementAppApp.swift
+//  BookMagagementApp
+//
+//  Created by Zhixuan Duan on 2024/4/28.
+//
+
+import SwiftUI
+
+@main
+struct BookMagagementAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
